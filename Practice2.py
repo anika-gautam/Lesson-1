ecf593f62424e118f8371b191462a8e94a56d5d6
anika-gautam <anika.vgautam@gmail.com>
2025-06-26 16:14:03 -0700

    quiz

---
+++ Practice2.py
@@ -0,0 +1,52 @@
+print("Welcome to the everything quiz")
+print("The questions are from different subjects")
+print("There are 5 questions to test your skills")
+print("Scores: 1.          2.        3.              4.                5."
+ "                                        Horrid Fail  Fail   Below Average    Average    Extremely Smart Pass.        "  )
+print("Good Luck!")
+y=input("How many continents are there in the world?  ")
+x=6 
+if (y==6):
+    print("That's Correct!") 
+else: 
+     print("Incorrect!") 
+print("Ha,ha if you wrote 7 that's the real correct answer")
+z=input( "What 1000+67 ")
+i= int(z)
+if (i==1067):
+     print("Correct!")
+else:
+     print("Incorrect")  
+a=input("Is Shriya crazy?") 
+
+if (a=="yes"):
+   print("Correct!")
+else:
+    print("Incorrect, also its sad you don't know Shriya that well")
+b=input("If you drop a ball on Earth will it fall to the ground?")
+if (b=="yes"):
+    print("Correct!")
+else: 
+    print("Incorrect!")
+c=input("Does Anika love you?")
+if (c=="banana"):
+   print("Correct!")
+else: 
+    print("No")
+print("You are so so right if you said yes")
+d=input("Your score:")
+j= int(d)
+if (j==5):
+   print("You are extremely smart!")
+if (j==4):
+   print("You are average")   
+if (j==3):
+   print("You are below average ")
+if (j==2): 
+   print("You failed")
+if (j==1):
+    print("I'm not going to even put the score ")
+print( "THANKS FOR PLAYING")
+
+   
+       
